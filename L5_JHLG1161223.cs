e6e8040266703188ecc014db6d27570a78298aa0
javierlopez19 <humberthojavier@gmail.com>
2026-02-18 19:04:53 -0600

    Update L5_JHLG1161223.cs

--- L5_JHLG1161223.cs
+++ L5_JHLG1161223.cs
@@ -1,1 +1,28 @@
-﻿a
+﻿class Class
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese el ID de usuario:");
+        int id = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el PIN: ");
+        int pin = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el Token: ");
+        int token = int.Parse(Console.ReadLine());
+
+        if ( id == 2026)
+        { Console.WriteLine("Usuario reconocido"); }
+        else { Console.WriteLine("usuario no reconocido"); }
+        if ( pin == 1234)
+        { Console.WriteLine("PIN correcto"); }
+        else { Console.WriteLine("PIN incorrecto"); }
+        if (token == 777)
+        { Console.WriteLine("Token válido"); }
+        else { Console.WriteLine("inválido"); } 
+
+
+
+    }
+}
+
+
+
